bd4db7f68498a7eb84c40f5ee3ef93e0c5e000c5
SwarnaKishore <swarnakishoree@gmail.com>
2021-04-22 08:07:18 -0500

    added my counter component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { StoreModule } from '@ngrx/store';
+import { counterReducer } from './counter.reducer';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyCounterComponent } from './my-counter/my-counter.component';
+// import { reducers, metaReducers } from './reducers';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, MyCounterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    StoreModule.forRoot({ count: counterReducer }),
+    AppRoutingModule,
+    // StoreModule.forRoot(reducers, {
+    //   metaReducers
+    // })
   ],
   providers: [],
   bootstrap: [AppComponent]
